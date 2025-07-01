3a8e261ec1862dcba82e23925d9581eb9a210b38
Sudheesh-07 <sudheeshshetty48@gmail.com>
2025-07-01 21:04:27 +0530

    minor changes

--- lib/src/features/authentication/view/get_started_page.dart
+++ lib/src/features/authentication/view/get_started_page.dart
@@ -3,7 +3,6 @@ import 'package:echo/src/core/extensions/context_extension.dart';
 import 'package:echo/src/core/extensions/image_extensions.dart';
 import 'package:echo/src/core/routes/app_routes.dart';
 import 'package:echo/src/core/utils/widgets/elevated_button.dart';
-import 'package:echo/src/core/utils/widgets/snackbarutils.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
@@ -138,9 +137,8 @@ class _GetStartedPageState extends State<GetStartedPage> {
                   EchoButton(
                     label: 'Log In',
                     onPressed:
-                        () => SnackbarUtils.showSuccess(context, 'Testing')
-                            //context.push(AppRoutes.authentication, extra: true),
-                            
+                        () =>
+                            context.push(AppRoutes.authentication, extra: true),
                   ),
                 ],
               ),
